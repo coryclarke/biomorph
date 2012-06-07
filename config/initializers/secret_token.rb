636fd39d191e7c7d466afd505657313753820f30
Cory Clarke <cory@tendercreative.com>
2012-06-07 11:02:14 -0400

    Adding initial Rails setup
    Need to reorganize views/HTML

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biomorph::Application.config.secret_token = 'f6c1bc3a7079d51c839280b3ab0fe7487e0786f5e31d0c49701860b9c5e163051bd6d31b85bd3c1cb7cbb4980a9188cb853d9231f6092b80b1b366465a03e576'
